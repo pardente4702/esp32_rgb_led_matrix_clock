8f6c2cf157700118c2d21cec7e299bdcdedd54f1
Paolo Ardente <paolo.ardente@gmail.com>
2026-06-21 11:35:33 +0200

    aggiunte credenziali modem di Ladispoli

--- include/WifiCredentials.h
+++ include/WifiCredentials.h
@@ -1,3 +1,9 @@
-// WifiCredentials.h  
-#define WIFI_SSID "FASTWEB-F7D7CB"  
-#define WIFI_PASSWORD "48WJAR7FWJ"
+// WifiCredentials.h
+
+// Credenziali modem Roma
+//#define WIFI_SSID "FASTWEB-F7D7CB"  
+//#define WIFI_PASSWORD "48WJAR7FWJ"
+
+// Credenziali modem Ladispoli
+#define WIFI_SSID "TIM_2rN62P"  
+#define WIFI_PASSWORD "4JTW3BQA66"
